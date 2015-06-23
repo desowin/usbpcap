4e82c1964f51694e40234b98a213161d64fba627
Tomasz Moń <desowin@gmail.com>
2015-06-23 22:13:43 +0300

    Initial WiX installer.

---
+++ wix/build.bat
@@ -0,0 +1,24 @@
+set _WIX_PATH="C:\Program Files (x86)\WiX Toolset v3.9\bin"
+
+cd %~dp0
+
+%_WIX_PATH%\candle.exe -nologo USBPcap.wxs -out USBPcap-x86.wixobj -ext WixUIExtension -arch x86 -dPlatform="x86"
+if errorlevel 1 goto error
+%_WIX_PATH%\light.exe -nologo USBPcap-x86.wixobj -out USBPcap-x86.msi -ext WixUIExtension
+if errorlevel 1 goto error
+pause
+
+%_WIX_PATH%\candle.exe -nologo USBPcap.wxs -out USBPcap-x64.wixobj -ext WixUIExtension -arch x64 -dPlatform="x64"
+if errorlevel 1 goto error
+%_WIX_PATH%\light.exe -nologo USBPcap-x64.wixobj -out USBPcap-x64.msi -ext WixUIExtension
+if errorlevel 1 goto error
+
+:error
+echo ===== Failed to build installer! =====
+pause
+exit /B 1
+
+:end
+echo ==== Success! ====
+pause
+exit /B 0
