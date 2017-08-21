dc98b4b797b651b447f909e7e102b949b72795af
Tomasz Moń <desowin@gmail.com>
2017-08-21 17:27:18 +0200

    Create Attestation signing cabinet files
    In order to get driver working on Windows 10 with Secure Boot enabled it
    is mandatory to upload the driver to Hardware Dev Center Dashboard for
    Attestation signing.
    
    Fixes: #39 and #43

--- build_release.bat
+++ build_release.bat
@@ -33,6 +33,16 @@ mkdir Release\Windows8\x64
 call cmd.exe /c driver_build_win8.bat x86 Release\Windows8\x86
 call cmd.exe /c driver_build_win8.bat x86_amd64 Release\Windows8\x64
 
+::Create Attestation signing cabinet files
+call cmd.exe /c driver_attestation_signing.bat
+@echo off
+echo Attestation signing packages should be available in disk1 directory
+echo Sign these files with EV Code Signing certificate and upload them
+echo to Hardware Dev Center Dashboard
+echo After you are ready, unzip signed files to Release directory
+@echo on
+pause
+
 ::Build finished
 
 @echo off
---
+++ driver_attestation_signing.bat
@@ -0,0 +1,37 @@
+cd %~dp0
+CALL config.bat
+
+CALL :dequote _USBPCAP_VERSION
+
+::Prepare driver package for Windows 10 Attestation signing
+::According to documentation Attestation signed driver works
+::only on Windows 10, hence we cannot use exactly the same
+::driver file for Windows 8 and Windows 10
+::However, we use the same source, only the signing differ
+CALL :CreateCab x86
+CALL :CreateCab x64
+Goto :eof
+
+:DeQuote
+for /f "delims=" %%A in ('echo %%%1%%') do set %1=%%~A
+Goto :eof
+
+:CreateCab
+(
+echo .OPTION EXPLICIT     ; Generate errors
+echo .Set CabinetFileCountThreshold=0
+echo .Set FolderFileCountThreshold=0
+echo .Set FolderSizeThreshold=0
+echo .Set MaxCabinetSize=0
+echo .Set MaxDiskFileCount=0
+echo .Set MaxDiskSize=0
+echo .Set CompressionType=MSZIP
+echo .Set Cabinet=on
+echo .Set Compress=on
+echo .Set CabinetNameTemplate=USBPcap-%_USBPCAP_VERSION%-%1.cab
+echo .Set DestinationDir=Windows10\%1
+echo %~dp0Release\Windows8\%1\USBPcap.inf
+echo %~dp0Release\Windows8\%1\USBPcap.sys
+) > Windows10-%1.ddf
+MakeCab /f Windows10-%1.ddf
+Goto :eof
