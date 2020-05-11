bc07695e8633131a20ff415bbe64526a0cfd4fd8
Mirosław Kozaczka <mkozaczka@gmail.com>
2020-05-11 08:23:00 +0200

    Update endpoint information on handle reuse (#98)
    If the same handle is reused as a result of SET INTERFACE/CONFIGURATION
    series, use the latest endpoint inormation associated with the handle.
    This results in correct endpoint information in captured packets.

--- USBPcapDriver/USBPcapTables.c
+++ USBPcapDriver/USBPcapTables.c
@@ -42,20 +42,24 @@ VOID USBPcapAddEndpointInfo(IN PRTL_GENERIC_TABLE table,
 {
     USBPCAP_INTERNAL_ENDPOINT_INFO  info;
     BOOLEAN                         new;
+    PUSBPCAP_INTERNAL_ENDPOINT_INFO pInfo;
 
     info.info.handle          = pipeInfo->PipeHandle;
     info.info.type            = pipeInfo->PipeType;
     info.info.endpointAddress = pipeInfo->EndpointAddress;
     info.info.deviceAddress   = deviceAddress;
 
-    RtlInsertElementGenericTable(table,
+    pInfo = RtlInsertElementGenericTable(table,
                                  (PVOID)&info,
                                  sizeof(USBPCAP_INTERNAL_ENDPOINT_INFO),
                                  &new);
 
-    if (new == FALSE)
+    if ((new == FALSE) && (pInfo != NULL))
     {
-        DkDbgStr("Element already exists in table");
+        DkDbgStr("Element already exists in table, updating entry");
+        pInfo->info.type            = pipeInfo->PipeType;
+        pInfo->info.endpointAddress = pipeInfo->EndpointAddress;
+        pInfo->info.deviceAddress   = deviceAddress;
     }
 }
 
